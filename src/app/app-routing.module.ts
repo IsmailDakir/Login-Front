230eb77f8de25f2f06a325b5f66b8b8103b5f223
130500 <130500@LAP-S-KAMMOUS>
2024-06-21 12:32:15 +0100

    first-commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from './Authentification/login/login.component';
+import { HomeComponent } from './Shared/home/home.component';
+import { AddTransferComponent } from './Shared/add-transfer/add-transfer.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/login', pathMatch: 'full' },
+  { path: 'login', component: LoginComponent },
+  { path: 'home',component: HomeComponent },
+  { path: 'AddTransfer',component: AddTransferComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,47 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule, provideClientHydration } from '@angular/platform-browser';
-
-import { AppRoutingModule } from './app-routing.module';
+import { HttpClientModule, provideHttpClient } from '@angular/common/http';
+import { FormsModule,ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './Authentification/login/login.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { HomeComponent } from './Shared/home/home.component';
+import { NgStyle } from '@angular/common';
+import { IconDirective } from '@coreui/icons-angular';
+import { ContainerComponent, RowComponent, ColComponent, CardGroupComponent, TextColorDirective, CardComponent, CardBodyComponent, FormDirective, InputGroupComponent, InputGroupTextDirective, FormControlDirective, ButtonDirective } from '@coreui/angular';
+import { AddTransferComponent } from './Shared/add-transfer/add-transfer.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    AddTransferComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    ContainerComponent,
+    RowComponent,
+    ColComponent,
+    CardGroupComponent,
+    TextColorDirective,
+    CardComponent,
+    CardBodyComponent,
+    FormDirective,
+    InputGroupComponent,
+    InputGroupTextDirective,
+    IconDirective,
+    FormControlDirective, 
+    ButtonDirective, 
+    NgStyle,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [
-    provideClientHydration()
+    provideHttpClient(),
   ],
   bootstrap: [AppComponent]
 })
